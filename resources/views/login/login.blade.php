4abd899b8be460c48ceb2162472a1d30f5ba5a10
Muhammad Rafli Abdillah <108709868+qwiqwest@users.noreply.github.com>
2023-02-08 14:25:23 +0700

    update login 08-02-2023

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,25 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!-- ==== STYLE.CSS ==== -->
-        <link rel="stylesheet" href="{{ asset('assets/css/stylel.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/stylel.css') }}" />
     <title>Login</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/image/flow.png') }}"/>
 </head>
 <body>
-    <form class="text-left" method="GET" action="/dashboard">
-        @csrf
-
-            <div class="login">
-                <div class="login-triangle"></div>
-                
-                <h2 class="login-header">Log in</h2>
-            
-                <form class="login-container">
-                <p><input type="email" placeholder="Email"></p>
-                <p><input type="password" placeholder="Password"></p>
-                <p><input type="submit" value="Login"></p>
-                </form>
+    <header>
+        <h1>CashFlow_ Login</h1>
+    </header>
+    <main>
+        <form id="login_form" class="form_class" action="/dashboard" method="get">
+            <div class="form_div">
+                <label>Login:</label>
+                <input class="field_class" name="login_txt" type="text" placeholder="Masukkan email" autofocus required>
+                <label>Password:</label>
+                <input id="pass" class="field_class" name="password_txt" type="password" placeholder="Masukkan Password" required>
+                <button class="submit_class" type="submit" form="login_form" onclick="return validarLogin()">Masuk</button>
             </div>
+        </form>
+    </main>
+    <footer>
+        <p>Fast Like a Flow <a href="#">CashFlow_&trade;</a></p>
+    </footer>
+    <script src="{{ asset('assets/js/scriptl.js') }}" defer></script>
 </body>
 </html>
