ed12ed531187923ea1cc6cf3c9e57e99b77d3322
Muhammad Rafli Abdillah <108709868+qwiqwest@users.noreply.github.com>
2023-02-15 08:44:54 +0700

    update referensi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,13 +21,24 @@ class DatabaseSeeder extends Seeder
         //seeder users
         $user = [ [
             'username' => 'Admin',
-            'name_lengkap' => 'AdminAllah',
+            'name_lengkap' => 'AdminBebas',
             'jk' => 'L',
             'alamat' => 'burangrang No.31 nomor 20',
             'nomor_tlp' => '085721077423',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin'),
             'role' => 'admin'
+        
+        ],
+        [
+            'username' => 'Kasie',
+            'name_lengkap' => 'KasirAdaAja',
+            'jk' => 'L',
+            'alamat' => 'simlim no 1',
+            'nomor_tlp' => '0812345678',
+            'email' => 'kasir@gmail.com',
+            'password' => Hash::make('kasir'),
+            'role' => 'kasir'
         
         ],
         ];
